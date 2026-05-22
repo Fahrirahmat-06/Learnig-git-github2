7f46bdd4804ce1a7ad86334051442397c0e1bfd1
2503010055-Fahri <2503010055@unper.ac.id>
2026-05-22 21:13:32 +0700

    Menambahkan h1 di file index.php

--- Index.php
+++ Index.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Coba git remote</title>
 </head>
+
 <body>
     <h1>Hello Dunia</h1>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit repudiandae doloremque odio vitae tempora
+    reprehenderit magni exercitationem, quod natus mollitia cumque culpa nostrum delectus? Culpa nemo itaque corrupti
+    natus ipsum!
 </body>
+
 </html>
